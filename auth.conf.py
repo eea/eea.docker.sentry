1f99367cf9a646f21ab15cac59874fc1ddc30edd
valentinab25 <30239069+valentinab25@users.noreply.github.com>
2020-03-18 16:55:17 +0200

    Configure correct ldap configuration

--- auth.conf.py
+++ auth.conf.py
@@ -1,18 +1,17 @@
+
 import ldap
-from django_auth_ldap.config import LDAPSearch, GroupOfUniqueNamesType, GroupOfNamesType, PosixGroupType, NestedGroupOfNamesType
+from django_auth_ldap.config import LDAPSearch, LDAPSearchUnion
 
 AUTH_LDAP_SERVER_URI = env('LDAP_SERVER', 'ldap://localhost')
 
 AUTH_LDAP_BIND_DN = env('LDAP_BIND_DN', '')
 AUTH_LDAP_BIND_PASSWORD = env('LDAP_BIND_PASSWORD', '')
+SENTRY_MANAGED_USER_FIELDS = ('email', 'first_name', 'last_name', 'password', )
 
-
-AUTH_LDAP_USER_SEARCH = LDAPSearch(
-    env('LDAP_USER_DN'),
-    ldap.SCOPE_SUBTREE,
-    env('LDAP_USER_FILTER', '(&(objectClass=inetOrgPerson)(cn=%(user)s))')
+AUTH_LDAP_USER_SEARCH = LDAPSearchUnion(
+    LDAPSearch(env('LDAP_USER_DN'), ldap.SCOPE_SUBTREE, "(uid=%(user)s)"),
+    LDAPSearch(env('LDAP_USER_DN'), ldap.SCOPE_SUBTREE, "(mail=%(user)s)"),
 )
-
 
 AUTH_LDAP_USER_ATTR_MAP = {
     'name': 'cn',
@@ -24,14 +23,10 @@ AUTH_LDAP_DEFAULT_SENTRY_ORGANIZATION = env(
     'eea')
 
 AUTH_LDAP_SENTRY_ORGANIZATION_ROLE_TYPE = 'member'
-AUTH_LDAP_SENTRY_SUBSCRIBE_BY_DEFAULT = False
+AUTH_LDAP_SENTRY_SUBSCRIBE_BY_DEFAULT = True
 AUTH_LDAP_SENTRY_ORGANIZATION_GLOBAL_ACCESS = True
-
 AUTH_LDAP_FIND_GROUP_PERMS = False
-
-
 AUTH_LDAP_SENTRY_USERNAME_FIELD = 'uid'
-
 
 AUTHENTICATION_BACKENDS = AUTHENTICATION_BACKENDS + (
     'sentry_ldap_auth.backend.SentryLdapBackend',
@@ -48,6 +43,3 @@ LOGGING['loggers']['django_auth_ldap'] = {
     'handlers': ['console'],
     'level': 'DEBUG'
 }
-
-
-
